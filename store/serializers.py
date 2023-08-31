fb6501173b981c8c1bb2d88b7f07d82743462850
Thomas Vu <thomas.vu@hotmail.com>
2023-09-01 00:32:28 +0200

    data validation

--- store/serializers.py
+++ store/serializers.py
@@ -16,3 +16,9 @@ class ProductSerializer(serializers.ModelSerializer):
 
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
+    
+    # def validate(self, data):
+    #     if data['password'] != data['confirm_password']:
+    #         return serializers.ValidationError('passwords do not match')
+    #     else:
+    #         return data
